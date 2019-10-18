768456d92af51ad968ce9fbac09d498cd325b2f4
Ajith <kanduriajith@gmail.com>
2019-10-18 13:04:20 +0530

    subscribed to topic

--- app/src/main/java/com/example/kartheek/controlzreview/MainActivity.java
+++ app/src/main/java/com/example/kartheek/controlzreview/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.kartheek.controlzreview;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.AudioAttributes;
+import android.media.RingtoneManager;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -37,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-
+        subscribeToNotifs();
         callFirebase();
     }
 
@@ -45,7 +54,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
     }
+    private  void subscribeToNotifs()
+    {
+        final SharedPreferences preferences = getSharedPreferences("MyPrefs",MODE_PRIVATE);
+        boolean flag = preferences.getBoolean("subscribed",false);
+        if(!flag) {
+            FirebaseMessaging.getInstance().subscribeToTopic("pushNotifications")
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            String msg = "Subscribed";
 
+                            if (!task.isSuccessful()) msg ="Subscription Failed";
+                            else preferences.edit().putBoolean("subscribed",true).apply();
+
+                            Log.d("MainActivity", msg);
+                            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel("Updates",
+                    "ATMOS Updates", NotificationManager.IMPORTANCE_HIGH);
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
+                    .build();
+            notificationChannel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),audioAttributes);
+            notificationChannel.setLockscreenVisibility(android.app.Notification.VISIBILITY_PUBLIC);
+            NotificationManager manager =(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+
+            assert manager!= null;
+            manager.createNotificationChannel(notificationChannel);
+        }
+
+    }
     private void callFirebase(){
         final DatabaseReference database = FirebaseDatabase.getInstance().getReference();
         final DatabaseReference notification = database.child("notification_review");
